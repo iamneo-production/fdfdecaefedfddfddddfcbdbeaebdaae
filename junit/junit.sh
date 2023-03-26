fa7e639b5d777fc9e9a9b28dbecc9a0452d098d7
root <root@fdfdecaefedfddfddddfcbdbeaebdaae-0.fdfdecaefedfddfddddfcbdbeaebdaae.che.svc.cluster.local>
2023-03-26 17:42:10 +0000

    Backup Branch before deleting

---
+++ junit/junit.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cp -r /home/coder/project/workspace/junit/test /home/coder/project/workspace/jspservlet-app-ATM/test/;
+cd /home/coder/project/workspace/jspservlet-app-ATM/;
+mvn clean test
